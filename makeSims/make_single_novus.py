3fc5a42ad72829170736d27feb720624c159c6a0
LucasKolanz <kolanzl@oregonstate.edu>
2024-01-12 11:12:24 -0800

    upgraded output and sim completion detection

--- makeSims/make_single_novus.py
+++ makeSims/make_single_novus.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
 	# attempts = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
 	attempts = [i for i in range(30)]
 	attempts_300 = [i for i in range(30)]
-	attempts = [1] 
+	attempts = [0] 
 	attempts_300 = attempts
 
 	#test it out first
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 	# attempts_300 = [0]
 
 	N = [30,100,300]
-	N = [5]
+	N = [30]
 	Temps = [3,10,30,100,300,1000]
 	Temps = [3]
 
@@ -73,63 +73,65 @@ if __name__ == '__main__':
 				else:
 					print("Job '{}' already exists.".format(job))
 
+				if os.path.exists(job+"timing.txt"):
+					print("Sim already complete")
+				else:
+					#load default input file
 
-				#load default input file
+					####################################
+					######Change input values here######
+					input_json['temp'] = Temp
+					input_json['seed'] = rand_int()
+					input_json['radiiDistribution'] = 'constant'
+					input_json['N'] = n
+					input_json['h_min'] = 0.5
+					input_json['dataFormat'] = "h5"
+					input_json['output_folder'] = job
+					# input_json['u_s'] = 0.5
+					# input_json['u_r'] = 0.5
+					input_json['note'] = "Rerunning constant size ball runs."
+					####################################
 
-				####################################
-				######Change input values here######
-				input_json['temp'] = Temp
-				input_json['seed'] = rand_int()
-				input_json['radiiDistribution'] = 'constant'
-				input_json['N'] = n
-				input_json['h_min'] = 0.5
-				input_json['dataFormat'] = "h5"
-				input_json['output_folder'] = job
-				# input_json['u_s'] = 0.5
-				# input_json['u_r'] = 0.5
-				input_json['note'] = "Rerunning constant size ball runs."
-				####################################
-
-				with open(job + "input.json",'w') as fp:
-					json.dump(input_json,fp,indent=4)
-
-
-				sbatchfile = ""
-				sbatchfile += "#!/bin/bash\n"
-				# sbatchfile += "#SBATCH -A m2651\n"
-				# sbatchfile += "#SBATCH -C gpu\n"
-				# sbatchfile += "#SBATCH -q regular\n"
-				# sbatchfile += "#SBATCH -t 0:10:00\n"
-				sbatchfile += "#SBATCH -J {}\n".format(job_set_name)
-				sbatchfile += "#SBATCH -N {}\n".format(1)#(node)
-				sbatchfile += "#SBATCH -n {}\n".format(1)#(node)
-				# sbatchfile += "#SBATCH -N {}\n".format(1)#(node)
-
-				# sbatchfile += "#SBATCH -G {}\n".format(node)
-				# sbatchfile += "#SBATCH -c {}\n\n".foramt(2*thread)
-				# sbatchfile += 'module load gpu\n'
-				# sbatchfile += 'export OMP_NUM_THREADS={}\n'.format(thread)
-
-				sbatchfile += 'export SLURM_CPU_BIND="cores"\n'
-				# sbatchfile += 'module load hdf5/1.14.3\n'
-				sbatchfile += 'module load hdf5/1.10.8\n'
-				
-				# sbatchfile += "srun -n {} -c {} --cpu-bind=cores numactl --interleave=all ./ColliderMultiCore.x {} 2>sim_err.log 1>sim_out.log".format(node,thread*2,job)
-				sbatchfile += f"srun {job}ColliderSingleCore.x {job} 2>sim_err.log 1>sim_out.log\n"
+					with open(job + "input.json",'w') as fp:
+						json.dump(input_json,fp,indent=4)
 
 
-				
-				with open(job+"sbatchMulti.bash",'w') as sfp:
-					sfp.write(sbatchfile)
+					sbatchfile = ""
+					sbatchfile += "#!/bin/bash\n"
+					# sbatchfile += "#SBATCH -A m2651\n"
+					# sbatchfile += "#SBATCH -C gpu\n"
+					# sbatchfile += "#SBATCH -q regular\n"
+					# sbatchfile += "#SBATCH -t 0:10:00\n"
+					sbatchfile += "#SBATCH -J {}\n".format(job_set_name)
+					sbatchfile += "#SBATCH -N {}\n".format(1)#(node)
+					sbatchfile += "#SBATCH -n {}\n".format(1)#(node)
+					# sbatchfile += "#SBATCH -N {}\n".format(1)#(node)
 
-				#add run script and executable to folders
-				# os.system(f"cp {project_path}default_files/run_sim.py {job}run_sim.py")
-				os.system(f"cp {project_path}ColliderSingleCore/ColliderSingleCore.x {job}ColliderSingleCore.x")
-				os.system(f"cp {project_path}ColliderSingleCore/ColliderSingleCore.cpp {job}ColliderSingleCore.cpp")
+					# sbatchfile += "#SBATCH -G {}\n".format(node)
+					# sbatchfile += "#SBATCH -c {}\n\n".foramt(2*thread)
+					# sbatchfile += 'module load gpu\n'
+					# sbatchfile += 'export OMP_NUM_THREADS={}\n'.format(thread)
 
-				os.system(f"cp {project_path}ColliderSingleCore/ball_group.hpp {job}ball_group.hpp")
+					sbatchfile += 'export SLURM_CPU_BIND="cores"\n'
+					# sbatchfile += 'module load hdf5/1.14.3\n'
+					sbatchfile += 'module load hdf5/1.10.8\n'
+					
+					# sbatchfile += "srun -n {} -c {} --cpu-bind=cores numactl --interleave=all ./ColliderMultiCore.x {} 2>sim_err.log 1>sim_out.log".format(node,thread*2,job)
+					sbatchfile += f"srun {job}ColliderSingleCore.x {job} 2>>sim_err.log 1>>sim_out.log\n"
 
-				folders.append(job)
+
+					
+					with open(job+"sbatchMulti.bash",'w') as sfp:
+						sfp.write(sbatchfile)
+
+					#add run script and executable to folders
+					# os.system(f"cp {project_path}default_files/run_sim.py {job}run_sim.py")
+					os.system(f"cp {project_path}ColliderSingleCore/ColliderSingleCore.x {job}ColliderSingleCore.x")
+					os.system(f"cp {project_path}ColliderSingleCore/ColliderSingleCore.cpp {job}ColliderSingleCore.cpp")
+
+					os.system(f"cp {project_path}ColliderSingleCore/ball_group.hpp {job}ball_group.hpp")
+
+					folders.append(job)
 
 print(folders)
 cwd = os.getcwd()
