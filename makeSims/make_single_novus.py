07b329be0f2850a7be24e8fa9e63ddf836508449
LucasKolanz <kolanzl@oregonstate.edu>
2024-01-12 12:17:13 -0800

    recreates error sim

--- makeSims/make_single_novus.py
+++ makeSims/make_single_novus.py
@@ -28,21 +28,17 @@ if __name__ == '__main__':
 	curr_folder = os.getcwd() + '/'
 
 	try:
-		# os.chdir("{}ColliderSingleCore".format(curr_folder))
 		subprocess.run(["make","-C",project_path+"ColliderSingleCore"], check=True)
 	except:
 		print('compilation failed')
 		exit(-1)
 
 
-	job_set_name = "const"
-	# folder_name_scheme = "T_"
+	job_set_name = "errorCk"
 
-	# runs_at_once = 7
-	# attempts = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
-	# attempts = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
-	attempts = [i for i in range(30)]
-	attempts_300 = [i for i in range(30)]
+ 
+	# attempts = [i for i in range(30)]
+	# attempts_300 = [i for i in range(30)]
 	attempts = [0] 
 	attempts_300 = attempts
 
@@ -50,9 +46,9 @@ if __name__ == '__main__':
 	# attempts = [0]
 	# attempts_300 = [0]
 
-	N = [30,100,300]
+	# N = [30,100,300]
 	N = [30]
-	Temps = [3,10,30,100,300,1000]
+	# Temps = [3,10,30,100,300,1000]
 	Temps = [3]
 
 	folders = []
