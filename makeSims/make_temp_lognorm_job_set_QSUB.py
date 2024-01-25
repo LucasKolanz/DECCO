a59a1d6eb0837b870f668cf2e601cd18f2378227
Lucas.Kolanz <kolanzl@oregonstate.edu>
2024-01-25 14:17:54 -0800

    u

--- makeSims/make_temp_lognorm_job_set_QSUB.py
+++ makeSims/make_temp_lognorm_job_set_QSUB.py
@@ -41,9 +41,9 @@ if __name__ == '__main__':
 	# runs_at_once = 7
 	# attempts = [21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
 	# attempts = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
-	attempts = [i for i in range(10,30)]
+	attempts = [i for i in range(0,30)]
 	# attempts = [1] 
-	attempts_300 = [i for i in range(5,13)]
+	attempts_300 = [i for i in range(0,15)]
 
 	N = [30,100,300]
 	# N = [3]
