fixed FD calculation code for different radii.

porosity_FD.py
@@ -160,10 +160,10 @@ if __name__ == '__main__':
 	# data_prefolder = path + 'jobs/h_max'
 	# data_prefolder = path + 'jobs/lognorm'
 	# data_prefolder = path + 'jobs/tempVarianceRand_attempt'
-	data_prefolder = path + 'jobsCosine/lognorm'
-	data_prefolder = path + 'jobsNovus/const'
-	data_prefolder = path + 'jobsOld/tempVarianceRand_attempt'
 	data_prefolder = path + 'jobsNovus/const_relax'
+	data_prefolder = path + 'jobsOld/tempVarianceRand_attempt'
+	data_prefolder = path + 'jobsNovus/const'
+	data_prefolder = path + 'jobsCosine/lognorm'
 
 	dataset_name = data_prefolder.split("/")[-1]
 
@@ -172,11 +172,10 @@ if __name__ == '__main__':
 	figure_folder = path+'data/figures/'
 
 
-	# temps = [10]
 	temps = [3,10,30,100,300,1000]
 	# temps = [3]
 	Nums = [30,100,300]
-	Nums = [30]
+	# Nums = [30]
 	
 	
 	attempts = [i for i in range(30)]
@@ -229,11 +228,11 @@ if __name__ == '__main__':
 	# yerr_FD[:] = np.nan
 	# yerr_ca[:] = np.nan
 
-	relax=True
+	relax = False
 	new_data = True     
-	save_data = False
+	save_data = True
 	show_plots = True
-	make_FD = False
+	make_FD = True
 	show_FD_plots = False
 	overwrite_octree_data = True 
 	find_stats = False
@@ -269,8 +268,8 @@ if __name__ == '__main__':
 					count = 0
 					if os.path.exists(data_folder):
 						if os.path.exists(data_folder+"timing.txt") or u.find_max_index(data_folder) >= N-3:
-							# porositiesabc[n,i,j] = porosity_measure1(data_folder,N-3)
-							# porositiesKBM[n,i,j] = porosity_measure2(data_folder,N-3)
+							porositiesabc[n,i,j] = porosity_measure1(data_folder,N-3)
+							porositiesKBM[n,i,j] = porosity_measure2(data_folder,N-3)
 							contacts[n,i,j] = number_of_contacts(data_folder,N-3,relax=relax)
 							angmom[n,i,j] = angular_momentum(data_folder,N-3,relax=relax)
 

utilities/utils.py
@@ -591,46 +591,32 @@ class datamgr(object):
 		print(pt1)
 		print(pt2)
 
-	#######################################################################################################
-	##NEED TO TEST 
-	#see if new version gives same output
+
 	def gen_whole_pt_cloud(self):
 		# self.orient_data()
 		# exit(0)
 		self.shift_to_first_quad()
 
+		accums = []
 		for ind,pt in enumerate(self.data):
 			radii = np.linspace(self.radius[ind]/100,self.radius[ind],100)
 
 			accum = [self.ppb*(radius**2/self.radius[ind]**2) for radius in radii]
 			accum = np.array(accum,dtype=int)
 			accum = np.where(accum < 100, 100, accum)
+			accums.append(accum)
 
-			return_array = np.zeros((self.data.shape[0]*np.sum(np.array(accum)),3))
+		return_array = np.zeros((np.sum(np.array(accums)),3))
+		for ind,pt in enumerate(self.data):
+			radii = np.linspace(self.radius[ind]/100,self.radius[ind],100)
+
 			for i,radius in enumerate(radii):
-				start_index = int(ind*np.sum(accum)) + np.sum(accum[:i])
-				end_index = int(ind*np.sum(accum)) + np.sum(accum[:i+1])
-				return_array[start_index:end_index] = self.gen_pt_cloud(pt,radius,accum[i])
+				start_index = int(ind*np.sum(accums[ind])) + np.sum(accums[ind][:i])
+				end_index = int(ind*np.sum(accums[ind])) + np.sum(accums[ind][:i+1])
+				return_array[start_index:end_index] = self.gen_pt_cloud(pt,radius,accums[ind][i])
+				
 		return return_array
-	# def gen_whole_pt_cloud(self):
-	# 	# self.orient_data()
-	# 	# exit(0)
-	# 	self.shift_to_first_quad()
 
-	# 	radii = np.linspace(self.radius[0]/100,self.radius[0],100)
-
-	# 	accum = [self.ppb*(radius**2/self.radius[0]**2) for radius in radii]
-	# 	accum = np.array(accum,dtype=int)
-	# 	accum = np.where(accum < 100, 100, accum)
-
-	# 	return_array = np.zeros((self.data.shape[0]*np.sum(np.array(accum)),3))
-	# 	for ind,pt in enumerate(self.data):
-	# 		for i,radius in enumerate(radii):
-	# 			start_index = int(ind*np.sum(accum)) + np.sum(accum[:i])
-	# 			end_index = int(ind*np.sum(accum)) + np.sum(accum[:i+1])
-	# 			return_array[start_index:end_index] = self.gen_pt_cloud(pt,radius,accum[i])
-	# 	return return_array
-	#######################################################################################################
 
 	#evenly spaced points on sphere code form:
 	#http://extremelearning.com.au/how-to-evenly-distribute-points-on-a-sphere-more-effectively-than-the-canonical-fibonacci-lattice/