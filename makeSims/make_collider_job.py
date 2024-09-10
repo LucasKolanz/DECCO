5651ec85b01890ac7178f79130df3424068bbbdb
LucasKolanz <kolanzl@oregonstate.edu>
2024-09-10 12:27:47 -0700

    Small make sim script changes

--- makeSims/make_collider_job.py
+++ makeSims/make_collider_job.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
 
 	runs_at_once = 1
 	# attempts = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
-	attempts = [1] 
+	attempts = [2] 
 	folders = []
 
 	MPInodes = 1
@@ -75,7 +75,7 @@ if __name__ == '__main__':
 		input_json['OMPthreads'] = threads
 		input_json['MPInodes'] = MPInodes
 
-		input_json['simTimeSeconds'] = 0.001
+		input_json['simTimeSeconds'] = 0.0001
 		input_json['timeResolution'] = 2e-4
 
 		input_json['seed'] = 100
